0b77b0e48261c2b2775b5c3e756f3b427534c42e
Leslie Muse <144291409+lmuse1@users.noreply.github.com>
2023-10-06 15:22:44 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>React Counter</h1>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
+
+
+//To start type npm start or http://localhost:3000/
+//Also and or type npm start
